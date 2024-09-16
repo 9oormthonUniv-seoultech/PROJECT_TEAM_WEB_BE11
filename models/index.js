6eeba487359881cda73764dbd6b2227d91610266
sooieese <sujinjang609@gmail.com>
2024-09-16 19:10:29 +0900

    feat: s3연결 및 리뷰작성api 생성

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const Sequelize = require('sequelize');
 
 // class 불러오기
 const User = require('./user');
+const Photobooth = require('./photobooth');
+const Review = require('./review');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../configs/config')[env];
@@ -16,5 +18,13 @@ db.Sequelize = Sequelize;
 db.User = User;
 User.init(sequelize);
 User.associate(db);
+
+db.Photobooth = Photobooth;  
+Photobooth.init(sequelize);
+Photobooth.associate(db);
+
+db.Review = Review;
+Review.init(sequelize);
+Review.associate(db);
 
 module.exports = db;
---
+++ models/review.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+
+class Review extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                user_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    comment: '유저 ID (foreign key)',
+                },
+                photobooth_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    comment: '포토부스 ID (foreign key)',
+                },
+                rating: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    comment: '평점',
+                },
+                booth_keyword: {
+                    type: Sequelize.JSON,
+                    allowNull: false,
+                    comment: '부스 관련 키워드',
+                },
+                photo_keyword: {
+                    type: Sequelize.JSON,
+                    allowNull: true,
+                    comment: '사진 관련 키워드',
+                },
+                date: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    comment: '리뷰 작성 날짜',
+                },
+                content: {
+                    type: Sequelize.STRING(600),
+                    allowNull: true,
+                    comment: '리뷰 내용 (최대 300자)',
+                },
+                image_url: {
+                    type: Sequelize.JSON,
+                    allowNull: true,
+                    comment: '리뷰 이미지의 S3 URL',
+                },
+            },
+            {
+                modelName: 'Review',
+                tableName: 'review',
+                charset: 'utf8mb4',
+                collate: 'utf8mb4_general_ci',
+                timestamps: true,
+                createdAt :false,
+                updatedAt : true,
+                paranoid: false,
+                underscored: false, 
+                sequelize,
+            }
+        );
+    }
+
+    static associate(db) {
+        // User 모델과의 관계 : 각 리뷰는 하나의 사용자에 속함(n:1)
+        db.Review.belongsTo(db.User, {
+            foreignKey: 'user_id',
+            targetKey: 'id', // User 테이블의 id와 연결
+            onDelete: 'RESTRICT', // User가 삭제될 때 리뷰는 삭제하지 않음
+        });
+        
+        // Photobooth 모델과의 관계 : 각 리뷰는 하나의 포토부스에 속함(n:1)
+        db.Review.belongsTo(db.Photobooth, {
+            foreignKey: 'photobooth_id', 
+            targetKey: 'id', // Photobooth 테이블의 id와 연결
+            onDelete: 'RESTRICT', // Photobooth가 삭제되면 해당 포토부스의 리뷰는 삭제하지 않음
+        });
+    }
+}
+
+module.exports = Review;
