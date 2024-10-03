367ac0d8d9a56f0a9f0a1f2329de0abfd83bf900
sooieese <sujinjang609@gmail.com>
2024-10-03 19:35:44 +0900

    feat:리뷰api, 미들웨어

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,10 +1,13 @@
 const Review = require('../models/review');
 const Photobooth = require('../models/photobooth');
+const { deleteImages} = require('../middlewares/s3');
+const { updatePhotobooth} = require('../middlewares/updatePhotobooth');
 
+// 리뷰 작성
 const createReview = async (req, res) => {
   try {
     const user_id = req.params.user_id;
-    const { photobooth_name, rating, booth_keyword, photo_keyword, content, date } = req.body;
+    const { photobooth_name, rating, booth_keyword, photo_keyword, content, date, imageUrls } = req.body;
     
     // 포토부스 이름을 통해 photobooth_id 조회
     const photobooth = await Photobooth.findOne({
@@ -16,12 +19,7 @@ const createReview = async (req, res) => {
       }
   
       const photobooth_id = photobooth.id;
-
-    // 이미지가 있으면 S3에 업로드 후 URL을 저장
-    let imageUrls = [];
-    if (req.files && req.files.length > 0) {
-      imageUrls = req.files.map(file => file.location); // S3에서 반환된 URL 저장
-    }
+      req.body.photobooth_id = photobooth_id;
 
     // 리뷰 생성
     const newReview = await Review.create({
@@ -31,15 +29,123 @@ const createReview = async (req, res) => {
       booth_keyword,
       photo_keyword,
       content,
-      image_url: imageUrls.length > 0 ? imageUrls : null,  // S3에서 반환된 URL 저장
+      image_url: imageUrls && imageUrls.length > 0 ? imageUrls : null,  // S3에서 반환된 URL 저장
       date: date,
     });
 
-    res.status(201).json(newReview);
+    await updatePhotobooth(req, res, () => {
+      res.status(201).json(newReview);
+    });
   } catch (error) {
     console.error('리뷰 생성 중 오류:', error);
     res.status(500).json({ message: '리뷰 생성에 실패했습니다.' });
   }
 };
 
-module.exports = { createReview };
+
+//리뷰 삭제
+const deleteReview = async (req, res) => {
+  try {
+      const review_id = req.params.review_id;
+      const review = await Review.findOne({
+          where: { id: review_id }
+      });
+
+      if (!review) {
+          return res.status(404).json({ message: '리뷰를 찾을 수 없습니다.' });
+      }
+
+      // 이미지 URL이 있으면 S3에서 삭제
+      if (review.image_url) {
+          const imageUrls = JSON.parse(review.image_url); 
+          await deleteImages(imageUrls); 
+      }
+   
+      const deletedReview = await Review.destroy({
+          where: { id: review_id } 
+      });
+
+      if (deletedReview) {
+        req.body.photobooth_id = review.photobooth_id;
+        await updatePhotobooth(req, res, () => {
+          res.status(200).json({ message: '리뷰가 삭제되었습니다.' });
+      });
+      } else {
+          res.status(404).json({ message: '리뷰를 찾을 수 없습니다.' });
+      }
+  } catch (error) {
+      res.status(500).json({ message: '리뷰 삭제에 실패했습니다.' });
+  }
+};
+
+//리뷰 상세정보 가져오기
+const getReviewDetail = async (req, res) => {
+  try {
+    const review_id = req.params.review_id;
+    const review = await Review.findOne({
+      where: { id: review_id },
+      include: [
+        {
+          model: Photobooth,
+          attributes: ['name', 'brand', 'rating']
+        }
+      ]
+    });
+    if (review) {
+      res.status(200).json(review);
+    } else {
+      res.status(404).json({ message: '리뷰를 찾을 수 없습니다.' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: '리뷰를 조회하는 중 오류가 발생했습니다.' });
+  }
+};
+
+
+//마이페이지 더보기 전 리뷰 2개 최신순 가져오기
+const getMypageReview = async(req, res) =>{
+  try {
+    const user_id = req.params.user_id;
+
+    const reviews = await Review.findAll({
+      where: { user_id: user_id },
+      order: [['date', 'DESC']],  
+      limit: 2,  
+      include: [
+        {
+          model: Photobooth,
+          attributes: ['name'], 
+        }
+      ]
+    });
+
+    // 리뷰가 없을 경우
+    if (reviews.length === 0) {
+      return res.status(200).json({
+          reviewNum: 0,
+          recent_reviews: null,
+      });
+  }
+
+    const review = reviews.map(review => ({
+      review_id:review.id,
+      date: review.date,
+      photobooth_name: review.Photobooth.name,  
+      rating: review.rating,
+      image: review.image_url ? JSON.parse(review.image_url)[0] : null
+    }));
+
+    const reviewNum = await Review.count({ where: { user_id: user_id } });
+
+    res.status(200).json({
+      reviewNum: reviewNum,
+      recent_reviews: review,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+
+}
+
+module.exports = { createReview, deleteReview, getReviewDetail, getMypageReview };
--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const kakaoStrategy = require('passport-kakao').Strategy;
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const { uploadProfileImage} = require('../services/s3Service'); 
+const { saveUploadOneImage} = require('../middlewares/s3'); 
 
 module.exports = () => {
 
@@ -32,13 +32,13 @@ module.exports = () => {
             } else {
                 //프로필이미지 s3에 저장, s3url가져오기
                 const profileImageUrl = profile._json.properties.profile_image;
-                const s3Url = await uploadProfileImage(profileImageUrl);
-
+                const s3Url = await saveUploadOneImage({ body: { imageUrl: profileImageUrl } });
+                
                 // 새로운 유저
                 const newUser = await User.create({
                     name: profile.displayName,
                     email: profile._json && profile._json.kakao_account.email,
-                    profileImage: s3Url,
+                    profileImage:  s3Url.s3Url,
                 });
 
                 const token = jwt.sign(
