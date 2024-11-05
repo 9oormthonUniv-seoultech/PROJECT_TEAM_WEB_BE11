a5948761332bbbaf40f922368e3886fe05f1d96d
sunyou10 <sy021023@gmail.com>
2024-11-05 21:36:13 +0900

    fix: 로그인 리다이렉트 주소 수정3

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ router.get('/kakao/callback', passport.authenticate('kakao', {
     const query = "?accessToken=" + token;
     res.locals.token = token;
 
-    res.redirect(`/auth/kakao`); // 로그인 성공 시
+    res.send({token, redirectUrl: "/home"}); // 로그인 성공 시
 });
 
 
