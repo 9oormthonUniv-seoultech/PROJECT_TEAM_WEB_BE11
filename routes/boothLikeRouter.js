3eed7d519b7a827825dc20ebb89d2587dd4934ad
sooieese <sujinjang609@gmail.com>
2024-11-07 23:52:02 +0900

    feat:부스위치api

--- routes/boothLikeRouter.js
+++ routes/boothLikeRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { addBoothLike, deleteBoothLike } = require('../controllers/boothLikeController');
-const {getBoothModal} = require('../controllers/boothPhotoController');
+const {getBoothModal, getBoothLocation} = require('../controllers/boothPhotoController');
 
 // 즐겨찾기 등록 라우트
 router.post('/', addBoothLike);
@@ -12,4 +12,6 @@ router.delete('/', deleteBoothLike);
 // 부스 모달창 가져오기 라우트
 router.get('/:photobooth_id', getBoothModal);
 
+// 부스 위치 가져오기 라우트
+router.get('/location/:photobooth_id', getBoothLocation);
 module.exports = router;
