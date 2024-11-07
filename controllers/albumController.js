935cb66bdfe4592a2dd30546d8d9ea4fe1a3163a
sunyou10 <sy021023@gmail.com>
2024-11-08 00:45:18 +0900

    fix: 위치 기반 앨범api 수정사항 반영, 문서화

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,13 +1,13 @@
 const Photo = require('../models/photo');
 const Photobooth = require('../models/photobooth');
 const { Sequelize, Op, fn, col } = require('sequelize');
+const axios = require('axios');
 
 const getAlbum = async (req, res) => {
   try {
     const user_id = req.params.user_id;
     const date = req.query.date ? new Date(req.query.date) : null; 
     const brand = req.query.brand ? req.query.brand : null;
-    const location = req.query.location ? req.query.location : null;
     const hashtag = req.query.hashtag ? req.query.hashtag : null; // 해시태그 검색 값 추가
 
     let photos;
@@ -39,17 +39,6 @@ const getAlbum = async (req, res) => {
             attributes: [],
           },
         ],
-        order: [['date', 'DESC']],
-        attributes: ['image_url', 'photo_like', 'id'],
-      });
-    } else if (location && req.nearbyBoothIds && req.nearbyBoothIds.length > 0) {
-      // location 필터링 - 미들웨어 반환값 이용, 현재위치 시에는 location=true로 받아야함
-      console.log("location으로 들어옴");
-      photos = await Photo.findAll({
-        where: {
-          user_id: user_id,
-          photobooth_id: { [Op.in]: req.nearbyBoothIds },
-        },
         order: [['date', 'DESC']],
         attributes: ['image_url', 'photo_like', 'id'],
       });
@@ -97,4 +86,74 @@ const getAlbum = async (req, res) => {
   }
 };
 
-module.exports = { getAlbum };
+const getNearAlbum = async (req, res) => {
+  const user_id = req.params.user_id;
+  let latitude, longitude;
+  if (searchTerm = req.query.searchTerm){
+    try {
+      const kakaoUrl = `https://dapi.kakao.com/v2/local/search/keyword.json?query=${searchTerm}`;
+      const kakaoSecretKey = process.env.KAKAO_ID;
+
+      const response = await axios.get(kakaoUrl, {
+          headers: {
+              Authorization: `KakaoAK ${kakaoSecretKey}`
+          }
+      });
+      console.log(response.data.documents[0].place_name);
+      longitude = response.data.documents[0].x;
+      latitude = response.data.documents[0].y;
+    } catch (err) {
+      console.error(err);
+      res.status(404).json({ message: '검색어의 위치를 찾을 수 없습니다.'});
+    }
+  } else {
+    latitude = req.query.latitude ? req.query.latitude : '37.6329741';
+    longitude = req.query.longitude ? req.query.longitude : '127.0798802';
+  }
+
+  console.log("latitude: ", latitude, "longitude: ", longitude)
+  const radius = 1000;
+
+  try {
+    const photobooths = await Photobooth.findAll({
+      where: Sequelize.where(
+          Sequelize.fn(
+              'ST_Distance_Sphere',
+              Sequelize.literal(`POINT(${longitude}, ${latitude})`),
+              Sequelize.literal(`POINT(longitude, latitude)`)
+          ),
+          {
+              [Op.lte]: radius
+          }
+      )
+    });
+
+    const boothIds = photobooths.map(booth => booth.id);
+
+    const photos = await Photo.findAll({
+      where: {
+        user_id: user_id,
+        photobooth_id: {
+          [Op.in]: boothIds
+        }
+      }
+    });
+
+    if (photos.length === 0) {
+      return res.status(200).json({ photonum: 0 });
+    };
+
+    const response = photos.map((photo) => ({
+      images: photo.image_url,
+      photo_like: photo.photo_like,
+      photo_id :photo.id ,
+    }));
+
+    return res.status(200).json(response);
+  } catch(err) {
+    console.error("getPhoto error", err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { getAlbum, getNearAlbum };
--- routes/albumRouter.js
+++ routes/albumRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getNearbyBooths } = require('../middlewares/location');
-const {getAlbum} = require('../controllers/albumController');
+const { getAlbum, getNearAlbum } = require('../controllers/albumController');
 
 // 앨범 조회용 라우트
 router.get('/:user_id', getNearbyBooths , getAlbum);
+router.get('/:user_id/location', getNearAlbum);
 
 module.exports = router;
